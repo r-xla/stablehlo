433d6a61fb03c73465b7160de3f01c7e6860f0c7
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-07-22 08:59:05 +0200

    feat: op subtract, use genereic inference

--- R/op-subtract.R
+++ R/op-subtract.R
@@ -7,6 +7,7 @@ hlo_subtract_impl <- hlo_fn(OpSubtract, infer_types_generic_biv)
 
 #' @templateVar mnemonic subtract
 #' @template op
+
 #' @export
 hlo_subtract <- function(lhs, rhs) {
   hlo_subtract_impl(values = list(lhs = lhs, rhs = rhs))
