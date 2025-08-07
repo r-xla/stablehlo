20808422fdf2d567240749eb229ae0f3a73bad23
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-08-07 15:55:35 +0200

    feat: Op Cosine

---
+++ R/op-cosine.R
@@ -0,0 +1,20 @@
+#' @include op.R hlo.R
+NULL
+
+Cosine <- new_Op("Cosine", "cosine")
+
+# binary ops
+infer_types_cosine <- function(operand) {
+  stopifnot(inherits(operand@type, TensorType))
+  ValueTypes(list(operand))
+}
+
+hlo_cosine_impl <- hlo_fn(Cosine, infer_types_cosine)
+
+#' @title element-wise ceil operation
+#' @param operand ([`FuncVariable`])
+#' @return [`FuncVariable`]
+#' @export
+hlo_cosine <- function(operand) {
+  hlo_cosine_impl(values = list(operand = operand))
+}
