ce9fa822e859fae2a250825d769be8a307ca099c
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-08-12 11:20:28 +0200

    feat: op-exponential / expoinentia_minus_one

---
+++ R/op-exponential_minus_one.R
@@ -0,0 +1,18 @@
+#' @include op.R hlo.R
+NULL
+
+Exponential_minus_one <- new_Op("Exponential_minus_one", "exponential_minus_one")
+
+infer_types_exponential_minus_one <- function(operand) {
+  stopifnot(inherits(operand@type, TensorType))
+  ValueTypes(list(operand))
+}
+
+hlo_exponential_minus_one_impl <- hlo_fn(Exponential_minus_one, infer_types_exponential_minus_one)
+
+#' @templateVar mnemonic exponential_minus_one
+#' @template op
+#' @export
+hlo_exponential_minus_one <- function(operand) {
+  hlo_exponential_minus_one_impl(values = list(operand = operand))
+}
