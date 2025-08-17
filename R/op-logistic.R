d084f61071dc11f476db5cea22c76b4db00ad195
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-08-17 19:26:04 +0200

    feat: op logistic

---
+++ R/op-logistic.R
@@ -0,0 +1,17 @@
+#' @include op.R hlo.R 
+NULL 
+
+Logistic <- new_Op("Logistic", "logistic")
+
+infer_types_logistic <- function(operand) {
+  stopifnot(inherits(operand@type, TensorType))
+  ValueTypes(list(operand))
+}
+hlo_logistic_impl <- hlo_fn(Logistic, infer_types_logistic) 
+
+#' @templateVar mnemonic logistic
+#' @template op
+#' @export
+hlo_logistic <- function(operand) {
+  hlo_logistic_impl(values = list(operand = operand))
+}
