7f35d0f87bbc423d01c309d2d2d3208182f8e6ba
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-08-17 18:14:39 +0200

    wrapper functions works for simple univariate and bivariate functions where condition lhs=rhs is satisfied

---
+++ R/wrapper/run.R
@@ -0,0 +1,30 @@
+# univariate functions
+univariates <- c(
+  "abs",
+  "cbrt",
+  "ceil",
+  "cosine",
+  "exponential_minus_one",
+  "exponential",
+  "floor",
+  "tan",
+  "tanh"
+)
+
+# create univariates: mind the right directory
+for (op in univariates) {
+  write_univariate_op(op)
+}
+
+# bivariate function where lhs=rhs
+bivariates <- c(
+  "add",
+  "atan2",
+  "subtract",
+  "divide"
+)
+
+# create biivariates: mind the right directory
+for (op in bivariates) {
+  write_bivariate_op(op)
+}
