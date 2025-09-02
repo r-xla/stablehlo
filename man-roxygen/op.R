5fc38abaabb868753c43ac8ebe4ca350e5b01190
Sebastian Fischer <sebf.fischer@gmail.com>
2025-09-02 17:12:21 +0200

    fix title

--- man-roxygen/op.R
+++ man-roxygen/op.R
@@ -1,6 +1,6 @@
 #' <% op <- get(paste0("Op", snake_to_camel(mnemonic))) %>
 #' <% f <- get(paste0("hlo_", mnemonic)) %>
-#' @title <%= op@name %> Operator
+#' @title <%= gsub("^Op", "", op@name) %> Operator
 #' @description
 #' See \url{https://openxla.org/stablehlo/spec#<%= mnemonic %>} for details.
 #' @param <%= paste(formalArgs(f), collapse = ",") %> ([`FuncVariable`])\cr
