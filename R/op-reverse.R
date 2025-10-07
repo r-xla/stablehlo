1000a9bd32782dcf7ee86302eeceaa8f0bb888ec
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-10-07 10:37:32 +0200

    feat: op reverse

---
+++ R/op-reverse.R
@@ -0,0 +1,71 @@
+#' @include op.R hlo.R
+NULL
+
+OpReverse <- new_Op("OpReverse", "reverse")
+
+infer_types_reverse <- function(
+  operand,
+  dimensions
+) {
+  stopifnot(inherits(operand@type, TensorType))
+
+  # Extract operand dims and target result dims
+  operand_dims <- shape(operand)
+  revdims <- dimensions@value@data
+
+  # (C2) is_unique(dimensions).
+  if (anyDuplicated(revdims) > 0) {
+    cli::cli_abort("dimensions must be unique")
+  }
+
+  # (C3) 0 <= dimensions < rank(result)
+  if (length(revdims) == 0L) {
+    cli::cli_abort(
+      "at least one dimension needs to be provided"
+    )
+  }
+  if (any(revdims < 0L | revdims >= length(operand_dims))) {
+    cli::cli_abort("dimensions must be within [0, rank(result))")
+  }
+
+  ValueTypes(list(
+    ValueType(
+      TensorType(
+        dtype = operand@type@dtype,
+        shape = Shape(operand_dims)
+      )
+    )
+  ))
+}
+
+hlo_reverse_impl <- hlo_fn(
+  OpReverse,
+  infer_types_reverse
+)
+
+#' @templateVar mnemonic reverse
+#' @template op
+#' @export
+hlo_reverse <- function(
+  operand,
+  dimensions
+) {
+  dim_attr <- hlo_tensor(as.integer(dimensions), dtype = "i64")
+
+  hlo_reverse_impl(
+    values = list(operand = operand),
+    attrs = list(dimensions = dim_attr)
+  )
+}
+
+method(repr, OpReverse) <- function(x) {
+  paste0(
+    repr(x@outputs),
+    " = ",
+    repr(x@name),
+    " ",
+    repr(x@inputs, simplify_dense = TRUE),
+    ": ",
+    repr(x@signature)
+  )
+}
