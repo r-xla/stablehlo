cf3ff884fe55743c07752a899a954d0cbc38b083
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-10-22 14:49:05 +0200

    feat sort

--- R/op-sort.R
+++ R/op-sort.R
@@ -6,7 +6,7 @@ OpSort <- new_Op("OpSort", "sort")
 infer_types_sort <- function(..., dimension, is_stable, comparator) {
   dots <- list(...)
   input_dims <- lapply(dots, \(x) shape(x))
-  dim <- dimension@value@data + 1
+  # dimension <- dimension@value@data + 1
 
   lapply(dots, function(x) {
     stopifnot(inherits(x, ValueType))
@@ -25,7 +25,10 @@ infer_types_sort <- function(..., dimension, is_stable, comparator) {
   }
 
   # (C4) -R <= dimension < R, where R = rank(inputs[0]).
-  if (dim > length(input_dims[[1]])) {
+  if (dimension > length(input_dims[[1]])) {
+    cli_abort("dimension must be present in inputs")
+  }
+  if (dimension < 1) {
     cli_abort("dimension must be present in inputs")
   }
 
@@ -43,16 +46,13 @@ hlo_sort_impl <- hlo_fn(OpSort, infer_types_sort)
 #' @export
 hlo_sort <- function(..., dimension, is_stable, comparator) {
   dots <- list(...)
-  dim_attr <- hlo_tensor(
-    as.integer(dimension),
-    dtype = "i64",
-    func = Func()
-  )
   hlo_sort_impl(
     values = dots,
     funcs = list(comparator = comparator),
-    attrs = list(dimension = dim_attr),
-    custom_attrs = list(is_stable = as.logical(is_stable))
+    custom_attrs = list(
+      dimension = as.integer(dimension),
+      is_stable = as.logical(is_stable)
+    )
   )
 }
 
@@ -62,14 +62,11 @@ method(repr, OpSort) <- function(x) {
     " = ",
     repr(x@name),
     " ",
-    sub(
-      "(.*?}.*?)\n}",
-      paste0(
-        "\\1,\nis_stable = ",
-        tolower(as.character(x@inputs@custom_attrs$is_stable)),
-        "\n} "
-      ),
-      repr(x@inputs, simplify_dense = TRUE, simplify_attrs = TRUE)
+    repr(x@inputs, simplify_dense = TRUE),
+    sprintf(
+      " {\ndimension = %d :i64,\nis_stable = %s\n} ",
+      x@inputs@custom_attrs$dimension - 1,
+      tolower(as.character(x@inputs@custom_attrs$is_stable))
     ),
     ": ",
     repr(x@signature)
--- tests/testthat/test-op-sort.R
+++ tests/testthat/test-op-sort.R
@@ -1,40 +1,49 @@
 test_that("basic tests", {
   local_func()
+  a1 <- hlo_input("arg1", "i32", shape = integer())
+  a2 <- hlo_input("arg2", "i32", shape = integer())
+  a3 <- hlo_input("arg3", "i32", shape = integer())
+  a4 <- hlo_input("arg4", "i32", shape = integer())
   comp <- hlo_compare(
-    hlo_input("lhs", "i32", shape = c(1, 3)),
-    hlo_input("rhs", "i32", shape = c(1, 3)),
+    a1,
+    a2,
     comparison_direction = "GT",
     compare_type = "SIGNED"
   )
   comp.func <- hlo_return(comp)
 
-  func <- local_func()
-  x1 <- hlo_input("x1", "i32", shape = c(2L, 3L))
-  x2 <- hlo_input("x2", "i32", shape = c(2L, 3L))
-  y <- hlo_sort(
-    x1,
-    x2,
-    dimension = 0L, # comply with zero-indexing
-    is_stable = TRUE,
-    comparator = comp.func
-  )
-  f <- hlo_return(y[[1]])
-  expect_snapshot(repr(f))
+  f <- list()
+  for (i in seq_len(2)) {
+    func <- local_func()
+    x1 <- hlo_input("x1", "i32", shape = c(2L, 3L))
+    x2 <- hlo_input("x2", "i32", shape = c(2L, 3L))
+    y <- hlo_sort(
+      x1,
+      x2,
+      dimension = 1L,
+      is_stable = TRUE,
+      comparator = comp.func
+    )
+    f[[i]] <- hlo_return(y[[i]], func = func)
+  }
+
+  expect_snapshot(lapply(f, repr))
 
   skip_if_not_installed("pjrt")
-  program <- pjrt_program(repr(f))
-  exec <- pjrt_compile(program)
+  program <- lapply(f, \(x) pjrt_program(repr(x)))
+  exec <- lapply(program, \(x) pjrt_compile(x))
 
-  x1 <- array(1L:3L, dim = c(3, 1))
-  x2 <- array(1L:6L, dim = c(3, 2))
-  x3 <- array(1L:9L, dim = c(3, 3))
-  expected <- array(cbind(x1, x2, x3), dim = c(3, 6))
+  x1 <- array(1L:6L * 10L, dim = c(2, 3))
+  x2 <- array(1L:6L * 10L + 1L, dim = c(2, 3))
 
-  output <- pjrt_execute(
-    exec,
-    pjrt_buffer(x1),
-    pjrt_buffer(x2),
-    pjrt_buffer(x3)
-  )
-  expect_equal(as_array(output), expected, tolerance = 1e-3)
+  expected <- list(x1[c(2, 1), ], x2[c(2, 1), ])
+
+  output <- lapply(exec, \(x) {
+    pjrt_execute(
+      x,
+      pjrt_buffer(x1),
+      pjrt_buffer(x2)
+    )
+  })
+  expect_equal(lapply(output, as_array), expected, tolerance = 1e-3)
 })
