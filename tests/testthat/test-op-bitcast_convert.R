940b68a929ac2f01dc1aae0ea9ab80cf3974a896
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-11-25 10:57:09 +0100

    tests for up-, down- and identitiy casting

--- tests/testthat/test-op-bitcast_convert.R
+++ tests/testthat/test-op-bitcast_convert.R
@@ -1,9 +1,39 @@
 test_that("basic tests", {
+  # debugonce(infer_types_bitcast_convert)
+
+  # Upcasting 1: i1 -> ui8
+  # throws error since last dimension should be 8, but is 16
   local_func()
-  x <- hlo_input("x", "f32", shape = c(2L, 3L))
+  x <- hlo_input("x", "i1", shape = c(2L, 3L, 16L))
+  expect_error(
+    y <- hlo_bitcast_convert(
+      x,
+      dtype = "ui8"
+    )
+  )
+
+  # this breaks R
+  # should convert i1 -> ui8
+  # # Upcasting 2
+  # local_func()
+  # x <- hlo_input("x", "i1", shape = c(2L, 3L, 8L))
+  # y <- hlo_bitcast_convert(
+  #   x,
+  #   dtype = "ui8"
+  # )
+  # f <- hlo_return(y)
+  # expect_snapshot(repr(f))
+  #
+  # skip_if_not_installed("pjrt")
+  # program <- pjrt_program(repr(f))
+  # exec <- pjrt_compile(program)
+
+  # Upcasting 2: i8 -> ui32
+  local_func()
+  x <- hlo_input("x", "i8", shape = c(2L, 3L, 4L))
   y <- hlo_bitcast_convert(
     x,
-    cast_to_dtype = "ui8"
+    dtype = "ui32"
   )
   f <- hlo_return(y)
   expect_snapshot(repr(f))
@@ -12,13 +42,56 @@ test_that("basic tests", {
   program <- pjrt_program(repr(f))
   exec <- pjrt_compile(program)
 
-  input <- array(seq(-1, 1, length.out = 6), dim = c(2, 3))
+  input <- array(as.integer(seq_len(24)), dim = c(2, 3, 4))
 
   output <- pjrt_execute(
     exec,
-    pjrt_buffer(input)
+    pjrt_buffer(input, dtype = "i8")
   )
 
-  expect_equal(dim(as_array(output)), c(2, 3, 4))
+  expect_equal(dim(as_array(output)), c(2, 3))
   expect_true(all(as_array(output) >= 0))
+
+  # identitiy casting ui32 -> f32
+  local_func()
+  x <- hlo_input("x", "ui32", shape = c(2L, 3L))
+  y <- hlo_bitcast_convert(
+    x,
+    dtype = "f32"
+  )
+  f <- hlo_return(y)
+  expect_snapshot(repr(f))
+
+  skip_if_not_installed("pjrt")
+  program <- pjrt_program(repr(f))
+  exec <- pjrt_compile(program)
+  input <- withr::with_seed(1, array(sample.int(1000, 6), dim = c(2, 3)))
+
+  output <- pjrt_execute(
+    exec,
+    pjrt_buffer(input, dtype = "ui32")
+  )
+  expect_equal(dim(as_array(output)), c(2, 3))
+  expect_true(all(as_array(output) <= 1e-41))
+
+  # downcasting: f64 -> f32
+  local_func()
+  x <- hlo_input("x", "f64", shape = c(2L, 3L))
+  y <- hlo_bitcast_convert(
+    x,
+    dtype = "f32"
+  )
+  f <- hlo_return(y)
+  expect_snapshot(repr(f))
+
+  skip_if_not_installed("pjrt")
+  program <- pjrt_program(repr(f))
+  exec <- pjrt_compile(program)
+  input <- array(rnorm(6), dim = c(2, 3))
+
+  output <- pjrt_execute(
+    exec,
+    pjrt_buffer(input, dtype = "f64")
+  )
+  expect_equal(dim(as_array(output)), c(2, 3, 2))
 })
