676a7e5f7f89d7ae06054d6c38b6e60988f999c5
Sebastian Fischer <sebf.fischer@gmail.com>
2025-11-25 08:59:01 +0100

    feat(op): re-add cholesky

---
+++ tests/testthat/test-op-cholesky.R
@@ -0,0 +1,24 @@
+test_that("basic tests", {
+  func <- local_func()
+  x <- hlo_input("x", "f64", shape = c(3L, 3L))
+  y <- hlo_cholesky(
+    x,
+    lower = FALSE
+  )
+  f <- hlo_return(y)
+  expect_snapshot(repr(f))
+
+  skip_if_not_installed("pjrt")
+  program <- pjrt_program(repr(f))
+  exec <- pjrt_compile(program)
+
+  M <- withr::with_seed(1, matrix(runif(9), 3, 3))
+  input <- t(M) %*% M
+  expected <- chol(input)
+
+  output <- pjrt_execute(exec, pjrt_buffer(input, dtype = "f64"))
+  # The content of the other half of the matrix is not guaranteed and backend dependent
+  output <- as_array(output)
+  output[lower.tri(output)] <- 0
+  expect_equal(output, expected, tolerance = 1e-3)
+})
