9edbd10712f859dacc05423cd092b2439ff85f04
Sebastian Fischer <sebf.fischer@gmail.com>
2025-12-30 11:22:10 +0100

    fix(op): clamp works for scalars (#129)

--- R/op-clamp.R
+++ R/op-clamp.R
@@ -8,8 +8,32 @@ OpClamp <- new_Op("OpClamp", "clamp")
 # binary ops
 infer_types_clamp <- function(Min, operand, Max) {
   assert_vts_are_tensors(Min = Min, operand = operand, Max = Max)
-  assert_vt_equal(operand, Max)
-  assert_vt_equal(Min, Max)
+
+  # (C3)
+  assert_vts_have_same_dtype(operand, Max)
+  assert_vts_have_same_dtype(Min, operand)
+
+  min_shape <- shape(Min)
+  operand_shape <- shape(operand)
+  max_shape <- shape(Max)
+
+  # (C1)
+  if (length(min_shape) != 0L && !identical(min_shape, operand_shape)) {
+    cli_abort(c(
+      "Min must have the same shape as operand or be a scalar.",
+      i = "Got Min shape {.val {min_shape}} and operand shape {.val {operand_shape}}."
+    ))
+  }
+
+  # (C1)
+  if (length(max_shape) != 0L && !identical(max_shape, operand_shape)) {
+    cli_abort(c(
+      "Max must have the same shape as operand or be a scalar.",
+      i = "Got Max shape {.val {max_shape}} and operand shape {.val {operand_shape}}."
+    ))
+  }
+
+  # (C4)
   ValueTypes(list(operand))
 }
 
--- tests/testthat/test-op-clamp.R
+++ tests/testthat/test-op-clamp.R
@@ -22,3 +22,25 @@ test_that("basic tests", {
   out_buf <- pjrt_execute(executable, lo_buf, x_buf, hi_buf)
   expect_equal(pmin(pmax(lo, x), hi), as_array(out_buf))
 })
+
+test_that("scalar min and max", {
+  skip_if_not_installed("pjrt")
+  func <- local_func()
+  x <- hlo_input("x", "f32", shape = c(2L, 2L))
+  x1 <- hlo_scalar(0.0, func = func)
+  x2 <- hlo_scalar(5.0, func = func)
+  y <- hlo_clamp(x1, x, x2)
+  result_func <- hlo_return(y)
+  expect_snapshot(repr(result_func))
+
+  program <- pjrt_program(repr(result_func))
+  expect_class(program, "PJRTProgram")
+
+  executable <- pjrt_compile(program)
+  expect_class(executable, "PJRTLoadedExecutable")
+
+  x <- array(c(-1, 2, 6, 3), dim = c(2, 2))
+  x_buf <- pjrt_buffer(x)
+  out_buf <- pjrt_execute(executable, x_buf)
+  expect_equal(pjrt_buffer(pmin(pmax(0.0, x), 5.0), shape = c(2, 2)), out_buf)
+})
