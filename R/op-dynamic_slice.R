6dc121a114965a686f09d59fda0bf15996d9032c
Sebastian Fischer <sebf.fischer@gmail.com>
2026-01-14 09:43:46 +0100

    error messages for (udpating) dynamic slices

--- R/op-dynamic_slice.R
+++ R/op-dynamic_slice.R
@@ -18,36 +18,50 @@ infer_types_dynamic_slice <- function(
 
   # (C2)
   if (length(start_indices) != operand_rank) {
-    cli_abort(
-      "size(start_indices) must equal rank(operand)"
-    )
+    cli_abort(c(
+      "size(start_indices) must equal rank(operand).",
+      i = "Got {length(start_indices)} start_indices and rank {operand_rank}."
+    ))
   }
 
   if (length(slice_sizes_data) != operand_rank) {
-    cli_abort(
-      "size(slice_sizes) must equal rank(operand)"
-    )
+    cli_abort(c(
+      "size(slice_sizes) must equal rank(operand).",
+      i = "Got {length(slice_sizes_data)} slice_sizes and rank {operand_rank}."
+    ))
   }
 
   # (C3)
   if (length(start_indices) > 0) {
     start_types <- lapply(start_indices, function(x) x$type)
     if (length(unique(start_types)) != 1) {
-      cli_abort("All start_indices must have the same type")
+      type_strs <- vapply(start_types, repr, character(1))
+      cli_abort(c(
+        "All start_indices must have the same type.",
+        i = "Got types: {paste0(type_strs, collapse = ', ')}."
+      ))
     }
 
     # Check that all start_indices are 0-dimensional tensors
     for (i in seq_along(start_indices)) {
       assert_vt_is_tensor(start_indices[[i]])
       if (length(shape(start_indices[[i]])) != 0) {
-        cli_abort("All start_indices must be 0-dimensional tensors")
+        error_unexpected_type(
+          arg = "start_indices",
+          index = i - 1L, # 0-based
+          expected = "must be 0-dimensional tensors",
+          actual = paste("shape", shapevec_repr(shape(start_indices[[i]])))
+        )
       }
     }
   }
 
   # (C4)
   if (any(slice_sizes_data > shape(operand))) {
-    cli_abort("slice_sizes must not be greater than operand's shape")
+    cli_abort(c(
+      "slice_sizes must not be greater than operand's shape.",
+      i = "Got slice_sizes {shapevec_repr(slice_sizes_data)} and operand shape {shapevec_repr(shape(operand))}."
+    ))
   }
 
   # (C1), (C5)
